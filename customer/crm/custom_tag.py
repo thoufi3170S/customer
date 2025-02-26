fea12753e07685b60dd9d134356da34c8e8f2ca1
thoufi3170S <thoufic787898@gmail.com>
2025-02-26 12:07:37 +0530

    Initial commit or update message

--- customer/crm/custom_tag.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def print_to_terminal(value):
-    print("Debug:", value)  # This prints to the terminal
-    return ""  # Returns an empty string to avoid breaking the template
